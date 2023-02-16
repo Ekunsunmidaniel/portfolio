b7ef8bf381d96164097d1ec5e45d269381e2a1d6
Joshua JJ <akhidenorjoshua@gmail.com>
2023-02-16 13:46:21 +0100

    Update files

--- js/script.js
+++ js/script.js
@@ -1,19 +1,46 @@
-const year = document.querySelector('.year');
+// Set Date
+const year = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 year.innerHTML = currentYear;
 
-const links = document.querySelectorAll('a');
+const links = document.querySelectorAll("a");
+const header = document.querySelector(".header");
+const sectionHero = document.querySelector('#section-hero')
 
-// links.forEach(link => {
-//     link.addEventListener('click', e => {
-//         e.preventDefault();
-//         const href = links.getAttribute('href');
 
-//         if(href === '#') {
-//             window.scrollTo({
-//                 top: 0,
-//                 behavior: 'smooth'
-//             });
-//         }
-//     })
-// });
+const btnNav = document.querySelector(".btn-mobile-nav");
+
+btnNav.addEventListener('click', function() {
+    header.classList.toggle('nav-open');
+    // sectionHero.classList.toggle('no-opacity');
+})
+
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    console.log(link);
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to link
+    if (href !== "#" && href.startsWith("#")) {
+      const section = document.querySelector(href);
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // // Close mobile navigation
+    if (link.classList.contains("nav-link")) {
+      header.classList.toggle("nav-open");
+    //   sectionHero.classList.toggle("no-opacity");
+      console.log('nav closed');
+    }
+  });
+});
+
